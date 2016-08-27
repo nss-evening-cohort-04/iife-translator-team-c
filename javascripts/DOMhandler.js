e7b7778f32deddd42f3adad5a77d557e54eed747
Alex Saber <Alex Saber>
2016-08-27 10:55:52 -0500

    added spanish.js

---
+++ javascripts/DOMhandler.js
@@ -0,0 +1,17 @@
+var selectLang = document.getElementById("language_selector");
+var translateBtn = document.getElementById("translate_button");
+translateBtn.addEventListener('click', function(event) {
+  var selectLangOptions = selectLang.options[selectLang.selectedIndex];
+  var langOptionValue = selectLangOptions.value;
+  console.log(langOptionValue);
+  toLanguage(langOptionValue);
+});
+
+function toLanguage(selected) {
+  var userInput = document.getElementById("userInputText").value;
+  var userInputArray = userInput.split(" ");
+  if(selected = "Spanish"){
+    Language.getSpanish(userInputArray);
+  }
+
+};
