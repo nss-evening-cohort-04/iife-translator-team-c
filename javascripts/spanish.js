789f4e998be416a798f3b57f31f6dbb289ed34c3
Alex Saber <Alex Saber>
2016-08-27 13:10:15 -0500

    fixed merge conflict

--- javascripts/spanish.js
+++ javascripts/spanish.js
@@ -1,22 +1,6 @@
 var Language = (function(makeSpanish) {
   var spanishObject = {
     Merry: "Feliz",
-<<<<<<< HEAD
-    Christmas: "Navidad"
-  };
-  makeSpanish.getSpanish = function(userText) {
-    for (var prop in spanishObject){
-      for (var i = 0; i < userText.length; i++){
-        if (userText[i] === prop);{
-          var translatedUserText = spanishObject[prop];
-        
-        }
-      }
-    }
-        console.log(spanishObject[prop]);
-  };
-
-=======
     Christmas: "Navidad",
     and: "y",
     a: "un",
@@ -35,6 +19,5 @@ var Language = (function(makeSpanish) {
     }
     Language.setLanguage(translatedArray);
   };
->>>>>>> 8ce96b1460e0f0afa6162ea243e8db054288a4eb
   return makeSpanish;
 })(Language || {});
