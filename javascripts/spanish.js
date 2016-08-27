e133ed243348f6a882f1d9763889bbef45d4a57c
henok tessema <heniselie@yahoo.com>
2016-08-27 12:03:10 -0500

    new stuff

---
+++ javascripts/spanish.js
@@ -0,0 +1,19 @@
+var Language = (function(makeSpanish) {
+  var spanishObject = {
+    Merry: "Feliz",
+    Christmas: "Navidad"
+  };
+  makeSpanish.getSpanish = function(userText) {
+    for (var prop in spanishObject){
+      for (var i = 0; i < userText.length; i++){
+        if (userText[i] === prop);{
+          var translatedUserText = spanishObject[prop];
+        
+        }
+      }
+    }
+        console.log(spanishObject[prop]);
+  };
+
+  return makeSpanish;
+})(Language || {});
